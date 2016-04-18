0f811cd8843facf7a795326fa68b044386549560
buffpojken <daniel@sykewarrior.com>
2016-04-18 15:05:45 +0200

    added more comments

--- reqrep.js
+++ reqrep.js
@@ -20,10 +20,24 @@ class ReqRepp{
         // I haven't read up on exactly what monitor does, but if it actually opens the connection, 
         // it should be moved to setup. 
         this.socket.monitor(10, 0);
+
+        // The reason I've moved the connection part out of the constructor is that it's bad design
+        // to mix a side-effect (opening a connection) with the creation of an object. That prevents
+        // any user of the module from for example making a req-rep object, confuigure it and then 
+        // pass it on to something asynchronous for later execution
+        // It's better to explicitly make the user of the module open the connection when they're good and ready!
     }
 
     setup(){
         this.socket.connect(this.port);
+
+        // There's no reason to construct an explicit promise-object and then return it, since
+        // there's no change in semantics with returning the promise straight away. That also
+        // prevents any mistakes resulting from copying/pasting which would change the order of 
+        // callbacks attached to the promise. 
+
+        // If the module itself needs to add callbacks to the promise when it resolves, then just
+        // add them and return a promise from the #then-callback straight away, as recommended by A+. 
         return new Promise((resolve, reject) => {
             this.socket.on('connect', (fd, ep) => {
                 resolve(this);
