07d3d40adab51529c547cc6369734b027685aa11
bambi <christopher.vigren@gmail.com>
2016-04-21 15:21:29 +0200

    reworked to handle timeouts

--- reqrep.js
+++ reqrep.js
@@ -1,6 +1,7 @@
 "use strict"; 
 var zmq              = require('zmq');
-var Promise          = require("bluebird");
+var Promise          = require('bluebird');
+var _                = require('underscore');
 
 const TYPE_READY     = "\x01";
 const TYPE_HEARTBEAT = "\x02";
@@ -8,57 +9,71 @@ const TYPE_REQUEST   = "\x03";
 const TYPE_RESPONSE  = "\x04";
 const TYPE_ERROR     = "\x05";
 
-class ReqRepp{
+class ReqRep {
 
-    constructor(port, options, tracking_id, method, message){
+    constructor(port, options) {
+        var self = this;
+
         this.port = port;
-        this.options = options;
+        this.options = _.extend({
+            connect_timeout: 1000,
+            receive_timeout: 1000
+        }, options);
 
         this.socket = zmq.socket('req');
         this.socket.identity = 'pid:' + process.pid;
 
-        // I haven't read up on exactly what monitor does, but if it actually opens the connection, 
-        // it should be moved to setup. 
-        this.socket.monitor(10, 0);
+        this.responses = [];
 
-        // The reason I've moved the connection part out of the constructor is that it's bad design
-        // to mix a side-effect (opening a connection) with the creation of an object. That prevents
-        // any user of the module from for example making a req-rep object, confuigure it and then 
-        // pass it on to something asynchronous for later execution
-        // It's better to explicitly make the user of the module open the connection when they're good and ready!
+        this.socket.on('error', function(err) {
+            var { resolve, reject} = self.responses.shift();
+
+            reject(err);
+        });
+
+        this.socket.on('message', function(type, tracking_id, data) {
+            var { resolve, reject} = self.responses.shift();
+
+            if (type.toString() == TYPE_RESPONSE) {
+                resolve(data);
+            } else {
+                reject(new Error("Incorrect response received."));
+            }
+        });
     }
 
-    setup(){
+    connect() {
+        this.socket.monitor(10, 0);
+
         this.socket.connect(this.port);
 
-        // There's no reason to construct an explicit promise-object and then return it, since
-        // there's no change in semantics with returning the promise straight away. That also
-        // prevents any mistakes resulting from copying/pasting which would change the order of 
-        // callbacks attached to the promise. 
-
-        // If the module itself needs to add callbacks to the promise when it resolves, then just
-        // add them and return a promise from the #then-callback straight away, as recommended by A+. 
         return new Promise((resolve, reject) => {
             this.socket.on('connect', (fd, ep) => {
+                resolve(this);
+            });
+        }).timeout(this.options.connect_timeout, 'connect timeout');
+    }
+
+    disconnect() {
+        this.socket.disconnect(this.port);
+
+        return new Promise((resolve, reject) => {
+            this.socket.on('disconnect', (fd, ep) => {
                 resolve(this);
             });
         });
     }
 
-    send(tracking_id, method, message){        
+    send(tracking_id, method, message) {
         this.socket.send([TYPE_REQUEST, tracking_id, method, message]);
 
         return new Promise((resolve, reject) => {
-            this.socket.on('message', function(type, tracking_id, data) {
-                if (type.toString() == TYPE_RESPONSE) {
-                    resolve(data);
-                } else {
-                    reject(new Error("Incorrect response received."));
-                }
+            this.responses.push({
+                resolve: resolve,
+                reject: reject
             });
-        });
+        }).timeout(this.options.receive_timeout, 'receive timeout');
     }
-
 }
 
-module.exports = ReqRepp;
+module.exports = ReqRep;
--- tests/base_test.js
+++ tests/base_test.js
@@ -1,60 +1,26 @@
-"use strict"; 
-const ReqRep     	= require('./../reqrep');
-const protobufjs 	= require("protobufjs");
-const node_rsa   	= require('node-rsa');
-const fs         	= require('fs');
-const assert 			= require("assert"); 
+"use strict";
+const ReqRep		= require('./../reqrep');
+const protobufjs	= require("protobufjs");
+const assert		= require("assert");
+const zmq			= require('zmq');
 
-// // protobuf-stuff
-// var builder = protobufjs.loadProtoFile("protobuf_formats/user_service/user_service.proto");
-// var pb_user_service = builder.build('user_service');
 
-// // key-stuff
-// var keyData = fs.readFileSync('dev.pem');
-// var key = new node_rsa();
-// key.importKey(keyData, 'pkcs1');
+describe("ReqRep with Relation Service", function() {
 
-// // connect!
-// var user_service = new ReqRep('tcp://accounts.test.lin.education:52002', {}).then(
-//     function(){
-//         console.log('conn ok!');
-
-//         var message = key.encrypt(new pb_user_service.Authenticate({
-//             'email': 'slask@slask.se',
-//             'password': '123'
-//         }).encode().toBuffer());
-
-//         // send message
-//         this.send('123', 'authenticate', message).then(
-//             function(data){
-//                 // decode response
-//                 console.log('ok!', pb_user_service.AuthenticationResponse.decode(data));
-//             },
-//             function(data){
-//                 // decode error
-//                 console.log('err', pb_user_service.AuthenticationError.decode(data));
-//             });
-//     },
-//     function(){
-//         console.log('conn err');
-//     });
-
-describe("ReqRep with Relation Service", function(){
-
-	before(function(){
+	before(function() {
 		this.pb_relation_service = protobufjs.loadProtoFile("protobuf_formats/relation_service/relation_service.proto").build('relation_service');
 	});
 
-	it('should load relations', function(done){
+	it('should load relations', function(done) {
 		var pb_relation_service = this.pb_relation_service;
-		var relation_service = new ReqRep('tcp://accounts.test.lin.education:52003', {}).setup().then(function(req){
+		var relation_service = new ReqRep('tcp://accounts.test.lin.education:52003', {}).connect().then(function(req) {
 			var message = new pb_relation_service.ReadUserRoleByUserUUID({
-		  	'user_uuid': '4548f758-ab78-11e3-bf6b-080027bf0f5d'
-		  }).encode().toBuffer();
-		  req.send('123', 'read_user_role_by_user_uuid', message).then(
-				function(data){
+				'user_uuid': '4548f758-ab78-11e3-bf6b-080027bf0f5d'
+			}).encode().toBuffer();
+			req.send('123', 'read_user_role_by_user_uuid', message).then(
+				function(data) {
 					done();
-			}).catch(done);
+				}).catch(done);
 		}).catch(done)
 	});
 
